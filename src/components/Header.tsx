513904bf3206a20e61cdbf4f388b81a513dcb9ee
sunil <skhatri15@outlook.com>
2022-09-05 14:01:53 -0400

    changed header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return (
         <div className="Header flex rounded-xl m-1 shadow-xl shadow-gray-900 mb-4 bg-gray-600">
-            <div className="Header-Items m-5 rounded-full w-full text-center">
-                <h1 className="text-white text-8xl m-4">  Project Portfolio </h1>
+            <div className="Header-Items overflow-hidden m-5 rounded-full w-full text-center">
+                <h1 className="text-white whitespace-nowrap text-7xl m-4">  Project Management </h1>
             </div>
         </div>
     );
